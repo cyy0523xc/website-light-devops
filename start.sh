c2b7173445fdc5ed7d75e0a4b50bb1ab5d5f216b
caiyingyao <cyy0523xc@gmail.com>
2021-08-10 16:59:25 +0800

    fix start sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
+# crontab:
+# * * * * * nohup bash /product_demo_devops/apps/website-light-devops/website-light-devops/start.sh &
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$dir"
 key="website-light-devops"
 if
-    ps aux|grep "$key"|grep -v grep
+    ps aux|grep "$key"|grep -v grep|grep start.sh
 then
     echo $(date) > status.log
 else

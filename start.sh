df2c08ab95077819d1b687406828ab6ad581400f
caiyingyao <cyy0523xc@gmail.com>
2021-08-11 10:32:37 +0800

    sh and readme

--- start.sh
+++ start.sh
@@ -2,12 +2,14 @@
 # * * * * * nohup bash /product_demo_devops/apps/website-light-devops/website-light-devops/start.sh &
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$dir"
-key="website-light-devops"
+# 尽量避免和目录名一致
+key="website-light-devops-run"
 if
     ps aux|grep "$key"|grep python|grep -v start.sh|grep -v grep
 then
     echo $(date) > status.log
     echo $(ps aux|grep "$key"|grep python|grep -v start.sh|grep -v grep) >> status.log
 else
+    echo $(date) >> running.log
     python3 src/main.py --key "$key" 1> running.log 2>&1  # >> running.log
 fi
